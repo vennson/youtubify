0e709b2a04264e0f58bde56ce2386aecf6e009a9
vennson <[bmiakoun@gmail.com](mailto:bmiakoun@gmail.com)>
2023-07-31 01:30:44 +0800

    add page title

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import SearchPage from './search'
 export default function Home() {
   return (
     <main>
+      <title>youtubify</title>
       <SearchPage />
     </main>
   )
--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -12,40 +12,56 @@ import { g, auth, config } from '@grafbase/sdk'
 // Define Data Models
 // https://grafbase.com/docs/database
 
-const post = g
-  .model('Post', {
-    title: g.string(),
-    slug: g.string().unique(),
-    content: g.string().optional(),
-    publishedAt: g.datetime().optional(),
-    comments: g
-      .relation(() => comment)
-      .optional()
-      .list()
-      .optional(),
-    likes: g.int().default(0),
-    tags: g.string().optional().list().length({ max: 5 }),
-    author: g.relation(() => user).optional(),
-  })
-  .search()
-
-const comment = g.model('Comment', {
-  post: g.relation(post),
-  body: g.string(),
-  likes: g.int().default(0),
-  author: g.relation(() => user).optional(),
+const video = g.model('Video', {
+  author: g.json(),
+  lengthSeconds: g.int(),
+  stats: g.json(),
+  thumbnails: g.json().list(),
+  title: g.string(),
+  videoId: g.string(),
+  votes: g.string().list(),
+  queue: g.relation(() => videoQueue),
 })
 
-const user = g.model('User', {
-  name: g.string(),
-  email: g.email().optional(),
-  posts: g.relation(post).optional().list(),
-  comments: g.relation(comment).optional().list(),
-
-  // Extend models with resolvers
-  // https://grafbase.com/docs/edge-gateway/resolvers
-  // gravatar: g.url().resolver('user/gravatar')
+const videoQueue = g.model('Queue', {
+  videos: g.relation(() => video).list(),
+  owner: g.string(),
 })
+
+// const post = g
+//   .model('Post', {
+//     title: g.string(),
+//     slug: g.string().unique(),
+//     content: g.string().optional(),
+//     publishedAt: g.datetime().optional(),
+//     comments: g
+//       .relation(() => comment)
+//       .optional()
+//       .list()
+//       .optional(),
+//     likes: g.int().default(0),
+//     tags: g.string().optional().list().length({ max: 5 }),
+//     author: g.relation(() => user).optional(),
+//   })
+//   .search()
+
+// const comment = g.model('Comment', {
+//   post: g.relation(post),
+//   body: g.string(),
+//   likes: g.int().default(0),
+//   author: g.relation(() => user).optional(),
+// })
+
+// const user = g.model('User', {
+//   name: g.string(),
+//   email: g.email().optional(),
+//   posts: g.relation(post).optional().list(),
+//   comments: g.relation(comment).optional().list(),
+
+//   // Extend models with resolvers
+//   // https://grafbase.com/docs/edge-gateway/resolvers
+//   // gravatar: g.url().resolver('user/gravatar')
+// })
 
 export default config({
   schema: g,
