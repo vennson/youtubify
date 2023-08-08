f4eccfcf12439f06643bfbd225553ce86ba4918e
vennson <[bmiakoun@gmail.com](mailto:bmiakoun@gmail.com)>
2023-08-09 05:41:36 +0800

    fix join room

--- graphql/actions.ts
+++ graphql/actions.ts
@@ -1,29 +1,6 @@
-import { GraphQLClient } from 'graphql-request'
-// import { getQueueQuery, getUserQuery } from '~/graphql/queries'
-import // createQueueMutation,
-// createUserMutation,
-// createVideoMutation,
-// deleteVideoMutation,
-// updateQueueMutation,
-// updateVideoMutation,
-'~/graphql/mutations'
-import { isProduction } from '~/lib/actions'
 import { useAppStore } from '~/store/store'
 import { apolloClient } from './client'
-import { GET_QUEUE, GET_USER, LIVE_GET_QUEUE } from './queries'
-
-const apiUrl = isProduction
-  ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ''
-  : 'http://127.0.0.1:4000/graphql'
-const apiKey = isProduction
-  ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ''
-  : 'letmein'
-
-const client = new GraphQLClient(apiUrl)
-
-const makeGraphQLRequest = async (query: string, variables = {}) => {
-  return await client.request(query, variables)
-}
+import { GET_QUEUE, GET_USER } from './queries'
 
 export async function getQueue(queueId: string) {
   const res = await apolloClient.query<QueueQueryResponse>({
@@ -31,52 +8,17 @@ export async function getQueue(queueId: string) {
     variables: { id: queueId },
   })
   return res.data.queue
-  // try {
-  //   const res = makeGraphQLRequest(getQueueQuery, {
-  //     id: queueId,
-  //   }) as Promise<QueueQueryResponse>
-  //   return res
-  // } catch (error) {
-  //   console.log('getQueue error', error)
-  // }
 }
 
 export async function joinRoomIfExists(roomId: string) {
   const { setQueueOwner } = useAppStore.getState()
 
   const queue = await getQueue(roomId)
-
   if (queue.id) {
     useAppStore.getState().setJoinedRoom(queue.id)
     setQueueOwner(queue.owner)
     return queue.id
   }
-}
-
-export async function refreshQueue() {
-  const { joinedRoom, setQueue, setQueueOwner, setNowPlaying, ownsQueue } =
-    useAppStore.getState()
-  if (!joinedRoom) return
-
-  // try {
-  //   const { queue: dbQueue } = (await getQueue(joinedRoom)) || {}
-
-  //   if (dbQueue) {
-  //     const freshVideos = dbQueue.videos.edges.filter((vid) => {
-  //       return !vid.node.isDone || !vid.node.isPlaying
-  //     })
-  //     setQueue(freshVideos)
-  //     setQueueOwner(dbQueue.owner)
-
-  //     if (!ownsQueue) {
-  //       setNowPlaying(dbQueue.nowPlaying)
-  //     }
-  //   }
-
-  //   return dbQueue
-  // } catch (error) {
-  //   console.log('refreshQueue error', error)
-  // }
 }
 
 export async function getUser(userId: string) {
--- graphql/client.ts
+++ graphql/client.ts
@@ -7,13 +7,21 @@ import {
 } from '@apollo/client'
 import { SSELink, isLiveQuery } from '@grafbase/apollo-link'
 import { getOperationAST } from 'graphql'
+import { isProduction } from '~/lib/actions'
 
 // export const apolloClient = new ApolloClient({
 //   uri: 'http://127.0.0.1:4000/graphql',
 //   cache: new InMemoryCache(),
 // })
 
-const uri = 'http://127.0.0.1:4000/graphql'
+// const uri = 'http://127.0.0.1:4000/graphql'
+
+const uri = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ''
+  : 'http://127.0.0.1:4000/graphql'
+const apiKey = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ''
+  : 'letmein'
 
 const initializeApolloClient = (link: ApolloLink) => {
   return new ApolloClient({
