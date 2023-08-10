8878f3a3ae908965252c1f6af68911de7c535199
vennson <[bmiakoun@gmail.com](mailto:bmiakoun@gmail.com)>
2023-08-10 09:07:52 +0800

    test prod

--- app/components/player/Player.tsx
+++ app/components/player/Player.tsx
@@ -118,6 +118,8 @@ export default function Player() {
     setLoading(false)
   }
 
+  console.log('pendingVideo', pendingVideo)
+
   return (
     <Box h={PLAYER_HEIGHT}>
       {!nowPlaying?.node.videoId && (
